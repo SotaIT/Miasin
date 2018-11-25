abd81abdff0b95c5c18129e83adc55049af23af2
aram-aramyan <aram_aramyan@mail.ru>
2018-11-26 00:46:09 +0300

    A sample mvc project added. Web project edited a little

---
+++ src/Miasin.Web/App_Start/Routes.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Miasin.Web
+{
+    public static class Routes
+    {
+        public static void ConfigureRoutes(this IRouteBuilder routes)
+        {
+            routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+}
---
+++ src/Miasin.Web/App_Start/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Identity.Dapper;
+using Identity.Dapper.Entities;
+using Identity.Dapper.PostgreSQL.Connections;
+using Identity.Dapper.PostgreSQL.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Miasin.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureDapperConnectionProvider<PostgreSqlConnectionProvider>(
+                Configuration.GetSection("ConnectionStrings"));
+
+            services.AddIdentity<DapperIdentityUser, DapperIdentityRole<int>>()
+                .AddDapperIdentityFor<PostgreSqlConfiguration>()
+                .AddDefaultTokenProviders();
+
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public static void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseMvc(routes => routes.ConfigureRoutes());
+        }
+    }
+}
