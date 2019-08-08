66acdbf9e68d03c4a549eecc57da4169d8280611
aram-aramyan <aram_aramyan@mail.ru>
2019-08-09 01:13:25 +0300

    hide admin panel

--- miasin.php
+++ miasin.php
@@ -8,10 +8,28 @@
  * License:           GPL2
  * Text Domain:       miasin
  * Domain Path:       /languages
+ * php version 7.3
+ 
+ * @category Plugins
+ * @package  Miasin
+ * @author   Aram Aramyan <aram_aramyan@mail.ru>
+ * @license  GPL2 https://github.com/SotaIT/Miasin/blob/master/LICENSE
+ * @link     http://miasin.org/
  */
 
- 
-// hide admin bar 
-if (!current_user_can('manage_options')) {
-    add_filter('show_admin_bar', '__return_false');
+
+add_action('init', 'miasin_init');
+
+
+/**
+ * Init
+ *
+ * @return void
+ */
+function Miasin_init()
+{
+    // hide admin bar 
+    if (!current_user_can('manage_options')) {
+        add_filter('show_admin_bar', '__return_false');
+    }
 }
